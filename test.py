889f35a301fa196814a6699931b1a82533bce07f
Kingsley Oteng <kingsley_oteng@outlook.com>
2024-01-06 18:44:30 -0500

    test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+# def square_digits(num):
+#     numbers: list = []
+#
+#     str_num: str = str(num)
+#     for c in str_num:
+#         square = int(c) ** 2
+#         numbers.append(str(square))
+#     return "".join(numbers)
+#
+#
+# print(square_digits(9119))
