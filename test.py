ee57a4f728d0a15a16a032809487594fbb81881c
Kingsley Oteng <kingsley_oteng@outlook.com>
2024-01-09 21:08:47 -0500

    Version 2

--- test.py
+++ test.py
@@ -9,13 +9,3 @@
 #
 #
 # print(square_digits(9119))
-
-def number(bus_stops):
-    total = 0
-    for people in bus_stops:
-        people_remaining = people[0] - people[1]
-        total += people_remaining
-
-    return total
-
-print(number([[10,0],[3,5],[5,8]]))
